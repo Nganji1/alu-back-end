d2d06898ef4a808dafbe1f35ede41c4566768b82
Nganji1 <J.nganji@alustudent.com>
2025-10-19 23:34:52 +0200

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 """gather data from an api"""
 import requests
 import sys
-
 if __name__ == '__main__':
     """gather data from an api"""
     response_todos = requests.get(
@@ -14,7 +12,6 @@ if __name__ == '__main__':
     for i in data:
         if i['completed']:
             completed_tasks.append(i['title'])
-
     user = requests.get(
         "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
     ).json()
